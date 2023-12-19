de5f34984b477282be40e65a531309d0d8b6c88a
Alessandra Regina da Silva <alessandraregina90@gmail.com>
2023-12-19 19:26:10 -0300

    primeiros arquivos

---
+++ desafios/aula2/desafio2.py
@@ -0,0 +1,10 @@
+# peso = float(input("Qual é o seu peso em quilos?"));
+# altura = float(input("Qual sua altura em metros?"));
+
+# imc = peso / (altura * altura);
+
+# print(f"Seu IMC é {imc}");
+
+numeros = []
+lista = (list(input["Insira 5 números separados por vírgulas: "]))
+print(lista)
